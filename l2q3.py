8dbffa3addefbafbd27411142835373c22ee12b6
vijaygosu <155065878+vijaygosu@users.noreply.github.com>
2024-03-11 23:40:45 +0530

    Update l2q3.py

--- l2q3.py
+++ l2q3.py
@@ -1,10 +1,10 @@
 def label_encoding(EX):
-    encoding={}
-    label=0
+    encoding = {}  # Initialize an empty dictionary
+    label = 0  # Initialize the label value
     for i in EX:
-            encoding[i]=label
-            label+=1
-    return encoding
+        encoding[i] = label  # Assign a label to each unique element in EX
+        label += 1  # Increment the label for the next element
+    return encoding  
 
-EX=['AB+','AB-','A+','A-']
-print("label encoding:",label_encoding(EX))
+EX = ['AB+', 'AB-', 'A+', 'A-']  
+print("label encoding:", label_encoding(EX))  
