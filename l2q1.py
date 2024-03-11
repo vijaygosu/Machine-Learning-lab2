3eda9c655e2997534fc89588410170d689303759
vijaygosu <155065878+vijaygosu@users.noreply.github.com>
2024-03-11 23:39:53 +0530

    Update l2q1.py

--- l2q1.py
+++ l2q1.py
@@ -1,18 +1,22 @@
-def ED(v1,v2):
-    ED=0
+def ED(v1, v2):
+    ED = 0  # Initialize Euclidean distance
     for i in range(len(v1)):
-        ED+=(v1[i]-v2[i])**2
-    return ED**0.5
+        ED += (v1[i] - v2[i])**2  # Add squared difference between corresponding elements
+    return ED**0.5  # Return square root of sum of squared differences
 
-def MD(v1,v2):
-    MD=0
+def MD(v1, v2):
+    MD = 0  # Initialize Manhattan distance
     for i in range(len(v1)):
-        MD+=(v1[i]-v2[i])
-    return MD
+        MD += v1[i] - v2[i]  # Add absolute difference between corresponding elements
+    return MD  # Return Manhattan distance
 
-v1=[1,2]
-v2=[1,3]
-ed=ED(v1,v2)
-md=MD(v1,v2)
-print("euclidean distance:",ed)
-print("manhattan distance:",md)
+v1 = [1, 2]
+v2 = [1, 3]
+
+# Calculate Euclidean distance and Manhattan distance
+ed = ED(v1, v2)
+md = MD(v1, v2)
+
+# Print the results
+print("euclidean distance:", ed)
+print("manhattan distance:", md)
