4501e49d771bf44f46de918a1586a0f204ff35d2
vijaygosu <155065878+vijaygosu@users.noreply.github.com>
2024-03-11 23:41:05 +0530

    Update l2q4.py

--- l2q4.py
+++ l2q4.py
@@ -1,10 +1,10 @@
 def onehot_encoding(EX):
-    encoding={}
+    encoding = {}  
     for i in EX:
-        encoding[i]=[0]*len(EX)
-        index=EX.index(i)
-        encoding[i][index]=1
-    return encoding
+        encoding[i] = [0] * len(EX)  # Create a list of zeros with length equal to the number of unique elements in EX
+        index = EX.index(i)  # Get the index of the current element in EX
+        encoding[i][index] = 1  # Set the corresponding index to 1 for the current element
+    return encoding  
 
-EX=['AB+','AB-','A+','A-']
-print("one-hot encoding:",onehot_encoding(EX))
+EX = ['AB+', 'AB-', 'A+', 'A-']  
+print("one-hot encoding:", onehot_encoding(EX))  
